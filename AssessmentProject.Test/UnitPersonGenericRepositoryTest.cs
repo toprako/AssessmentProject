67df16edf74e4b7a498e4338f174522af98dbad5
Mert Toprak <merttoprk58@gmail.com>
2023-11-03 10:40:47 +0300

    Unit Test Güncellemeleri

---
+++ AssessmentProject.Test/UnitPersonGenericRepositoryTest.cs
@@ -0,0 +1,130 @@
+﻿using DataAccessLayer.Concrete;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentProject.Test
+{
+    public class UnitPersonGenericRepositoryTest
+    {
+        #region Variable
+        private readonly GenericRepository<Person> genericRepository;
+        private readonly RepositoryWrapper repositoryWrapper;
+        #endregion
+
+        public UnitPersonGenericRepositoryTest()
+        {
+            var options = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseInMemoryDatabase(databaseName: "FakeDatabase")
+            .Options;
+            RepositoryContext context = new RepositoryContext(options);
+            genericRepository = new GenericRepository<Person>(context);
+            repositoryWrapper = new RepositoryWrapper(context);
+        }
+
+
+        [Fact]
+        public void Delete_ReturnEmptyMessage()
+        { 
+            //Arrange 
+            Person person = new Person()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test",
+                SurName = "Test",
+            };
+            var responseSave = genericRepository.Insert(person);
+            repositoryWrapper.Save();
+
+            //Act
+            var response = genericRepository.Delete(person);
+            repositoryWrapper.Save();
+
+            //Assert
+            Assert.Equal(string.Empty, response);
+        }
+
+        [Fact]
+        public void Insert_ReturnErrorMessage()
+        { 
+            //Arrange 
+            Person person = new Person()
+            {
+                Id = Guid.NewGuid(),
+                SurName = "b"
+            };
+
+            //Act
+            var response = genericRepository.Insert(person);
+
+            //Assert
+            Assert.Throws<DbUpdateException>(() => repositoryWrapper.Save());
+        }
+
+        [Fact]
+        public void Insert_ReturnOkMessage()
+        {
+            //Arrange 
+            Person person = new Person()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test",
+                SurName = "Test",
+            };
+
+            //Act
+            var response = genericRepository.Insert(person);
+            repositoryWrapper.Save();
+
+            //Assert
+            Assert.Equal(string.Empty, response);
+        }
+
+        [Fact]
+        public void Update_ReturnOkMessage()
+        { 
+            //Arrange 
+            Person person = new Person()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test",
+                SurName = "Test",
+            };
+            var responseSave = genericRepository.Insert(person);
+            repositoryWrapper.Save();
+            person.SurName = "AABB";
+
+            //Act
+            var response = genericRepository.Update(person);
+            repositoryWrapper.Save();
+
+            //Assert
+            Assert.Equal(string.Empty, response);
+        }
+
+        [Fact]
+        public void GetById_ReturnOkMessage()
+        {
+            //Arrange 
+            Person person = new Person()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test",
+                SurName = "Test",
+            };
+            var responseSave = genericRepository.Insert(person);
+            repositoryWrapper.Save();
+
+            //Act
+            var response = genericRepository.GetById(person.Id);
+
+            //Assert
+            Assert.NotNull(response);
+        }
+    }
+}
---
+++ AssessmentProject.Test/UnitPersonServiceExtensionTest.cs
@@ -0,0 +1,46 @@
+﻿using AssessmentProject.Persons.Extensions;
+using FakeItEasy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentProject.Test
+{
+    public class UnitPersonServiceExtensionTest
+    {
+
+        [Fact]
+        public static void ConfigureCors_Test()
+        {
+            var service = A.Fake<IServiceCollection>();
+            ServiceExtensions.ConfigureCors(service);
+        }
+
+        [Fact]
+        public static void ConfigurePostgreSqlContext_Test()
+        {
+            var service = A.Fake<IServiceCollection>();
+            var config = A.Fake<IConfiguration>();
+            ServiceExtensions.ConfigurePostgreSqlContext(service, config);
+        }
+
+        [Fact]
+        public static void ConfigureRepositoryWrapper_Test()
+        {
+            var service = A.Fake<IServiceCollection>();
+            ServiceExtensions.ConfigureRepositoryWrapper(service);
+        }
+
+        [Fact]
+        public static void ConfigureRabbitMq_Test()
+        {
+            var service = A.Fake<IServiceCollection>();
+            var config = A.Fake<IConfiguration>();
+            ServiceExtensions.ConfigureRabbitMq(service, config);
+        }
+    }
+}
